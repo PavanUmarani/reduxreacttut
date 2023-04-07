c14854600e8f51a5ad11e42968524446e8aa1495
PavanUmarani <pavanumarani123@gmail.com>
2023-04-07 19:28:58 +0530

    reducer file pushed

--- src/Services/Actions/action.js
+++ src/Services/Actions/action.js
@@ -1,4 +1,5 @@
 import { ADD_TO_CART } from "../Constants"
+import { REMOVE_TO_CART } from "../Constants";
 export const addToCart = (data) =>{
     return{
         type:ADD_TO_CART,
@@ -6,3 +7,9 @@ export const addToCart = (data) =>{
     }
 
 }
+export const removeToCart = (data) => {
+  return {
+    type: REMOVE_TO_CART,
+    data: data,
+  };
+};
